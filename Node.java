2ff7a2f5647e9a0434615822348d90bf010365e6
GeorgeVolochaev <107146884+GeorgeVolochaev@users.noreply.github.com>
2022-11-07 21:39:01 +0400

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,24 @@
+
+public class Node {
+    int data;
+    Node next;
+    
+	public Node(int data, Node next) {
+		super();
+		this.data = data;
+		this.next = next;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [data=" + data + ", next=" + next + "]";
+	}
+
+	
+
+	
+    
+    
+    
+	
+}
